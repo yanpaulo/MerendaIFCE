7d221fed2fd7aa5bbdef6bf38f691e2c71f11ce6
Yan Paulo <yanpaulo@hotmail.com>
2019-03-20 08:11:47 -0300

    Layout for UWP

---
+++ MerendaIFCE.UserApp/MerendaIFCE.UserApp/MarkupExtensions/ImagePathExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MerendaIFCE.UserApp.MarkupExtensions
+{
+    [ContentProperty(nameof(Path))]
+    public class ImagePathExtension : IMarkupExtension
+    {
+        public string Path { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    return Path.Replace("-", "_");
+                case Device.iOS:
+                    return Path;
+                case Device.UWP:
+                    return $"Assets/{Path}";
+                default:
+                    return new InvalidOperationException("Trying to get image path on unsupported Device type.");
+            }
+        }
+    }
+}
